19183f5561f0f886cbfce3a65e1c15c2d415510a
sofarinal <sofa.markis@yandex.ru>
2026-02-18 17:42:00 +0300

    дз 1

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,31 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Author author1 = new Author("Юлия", "Вереск");
+        Author author2 = new Author("Микита", "Франко");
+        Author author3 = new Author("Лев", "Толстой");
+
+        Book book1 = new Book("Тот самый", author1, 2020);
+        Book book2 = new Book("Девочка в нуливой степени", author2, 2020);
+        Book book3 = new Book("Война и мир", author3, 1877);
+
+        System.out.println("Книги до измненения");
+
+        printBookInfo(book1);
+        printBookInfo(book2);
+        printBookInfo(book3);
+
+        System.out.println("Книги после изменения");
+        printBookInfo(book1);
+        printBookInfo(book2);
+        printBookInfo(book3);
+    }
+
+    public static void printBookInfo(Book book) {
+        System.out.println("Книга: " + book.getTitle());
+        System.out.println("Автор: " + book.getAuthor().getName() + " " + book.getAuthor().getSurname());
+        System.out.println("Год публикации: " + book.getPublicationYear());
+        System.out.println();
     }
 }
